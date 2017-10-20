ff735b55fd6592464ebb4deb2cddf81de6ac0f48
TerryMinal <tguan@stuy.edu>
2017-10-19 21:45:25 -0400

    updated headers factoring in array

--- music_library.h
+++ music_library.h
@@ -1,24 +1,24 @@
 struct node;
 // Add song node
-struct node * add_song_node(struct node *song);
+struct node * add_song_node(char lib[], struct node *song);
 
 // search for a song given song and artist name
-struct node * search(char artist[], char song[]);
+struct node * search(char lib[], char artist[], char song[]);
 
 // print out all entries under a certain letter
-void print_under_letter(char letter);
+void print_under_letter(char lib[], char letter);
 
 // print out all songs of a specific artist
-void print_artist_song(char artist[]);
+void print_artist_song(char lib[], char artist[]);
 
 // prints out the whole library
-void print_library();
+void print_library(char lib[]);
 
 // print out a series of randomly chosen songs
-void shuffle();
+void shuffle(char lib[]);
 
 // delete a song
-void delete_song(char song[]);
+void delete_song(char lib[], char song[]);
 
 // delete all nodes
-void delete_nodes();
+void delete_nodes(char lib[]);
